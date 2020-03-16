4ecdb45670786ed6d428b524acad65d928d1a799
k1ndS2 <61953345+k1ndS2@users.noreply.github.com>
2020-03-17 00:57:01 +0900

    Update [17144]goodbye_finedust.py

--- baekjoon/[17144]goodbye_finedust.py
+++ baekjoon/[17144]goodbye_finedust.py
@@ -4,6 +4,7 @@ def print_area(a):
     for l in a:
         print(l)
 
+
 def upper_shift(a, idx1):
     for r in reversed(range(idx1-1)):
         a[r+1][0]=a[r][0]
@@ -15,19 +16,17 @@ def upper_shift(a, idx1):
     for i in range(2,len(a[0])):
         a[idx1][i]=a[idx1][i-1]
     a[idx1][1]=0
+
     
 def lower_shift(a, idx2):
     max_r = len(a) -1 
     max_c = len(a[0])-1
     for r in range(idx2+1, max_r):
         a[r][0] = a[r+1][0]    
-
     for i in range(0,max_c):
         a[max_r][i]=a[max_r][i+1]
-
     for r in reversed(range(idx2,max_r)):
-        a[r+1][max_c]=a[r][max_c]
-        
+        a[r+1][max_c]=a[r][max_c]        
     for i in range(1,max_c):
         a[idx2][i+1]=a[idx2][i]
     a[idx2][1]=0           
@@ -37,28 +36,9 @@ def aircleaner(a, lo_idx):
     upper_shift(a, lo_idx)
     lower_shift(a, lo_idx+1)
 
-def finedust_spreads(a, r, c, acidx1, acidx2):
-    pass
 
-if __name__=="__main__":
-    r, c, t = map(int, input().split())
-    area = list()
-    result = list()
-    for i in range(r):
-        area.append(list(map(int, input().split())))
-
+def finedust_spreads(area, r, c, acidx1, acidx2):
     result = copy.deepcopy(area)
-    acidx1 = -1
-    acidx2 = -1
-    #result = list(area)
-    print_area(area)
-    # check where air cleaner is located
-    for k in range(r):
-        if area[k][0]==-1:
-            acidx1 = k
-            acidx2 = k+1
-            break
-    
     # finedust spreads 
     for i in range(r):
         for j in range(c):
@@ -91,6 +71,7 @@ if __name__=="__main__":
             # last col
             if j==c-1:    
                 count -= 1
+                result[i][j-1] = result[i][j-1] + spread
             else:
                 if not j:
                     count-=1
@@ -142,5 +123,33 @@ if __name__=="__main__":
 
     print(f"+++ RESULT +++")
     print_area(result)
+    return result
 
 
+if __name__=="__main__":
+    r, c, t = map(int, input().split())
+    area = list()
+    sum_area = 0 
+#    result = list()
+    for i in range(r):
+        area.append(list(map(int, input().split())))
+
+#    result = copy.deepcopy(area)
+    acidx1 = -1
+    acidx2 = -1
+    #result = list(area)
+    print_area(area)
+    # check where air cleaner is located
+    for k in range(r):
+        if area[k][0]==-1:
+            acidx1 = k
+            acidx2 = k+1
+            break
+
+    for i in range(t):
+        area = copy.deepcopy(finedust_spreads(area,r,c,acidx1,acidx2))
+
+    for i in range(r):
+        sum_area += sum(area[i])
+
+    print(f"finedust_sum = {sum_area+2}")
