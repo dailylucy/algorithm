d4396399527b4d314a0fbdcf9d1d1a1f29ad5612
k1ndS2 <61953345+k1ndS2@users.noreply.github.com>
2020-03-19 22:41:11 +0900

    Update [2606]virus.py

--- baekjoon/[2606]virus.py
+++ baekjoon/[2606]virus.py
@@ -1,50 +1,37 @@
 #import time
-
-def recur_worm(n, k, v):
-    #print(f"[+] RECURSION STARTED {k}")
-    sum = 0
-    if k in n.keys():
-        for i in n[k]:
-            if i not in v:
-                sum+=recur_worm(n,i,list(set(v + n[k])))
-    sum+=1
-    return sum
-
+import queue
 
 if __name__ == "__main__":
     
-    count = 0
-    network = dict()
     computers = int(input())
+    network = [[]]*computers
     pairs = int(input())
-
-    # s = time.time()
-    # adjecent nodes info
-    # 개선1 : elif문으로 요소 중복 없도록 저장하였다가 else문으로 바꾸고 list(set())으로 변경
-    # >> 미미한 감소.. 그래도 입력 pair 값이 많아지면 좀 효과가 있지 않을까.
+    q = queue.Queue()
+    
+    #s = time.time()
     for i in range(pairs):
         a, b = map(int, input().split())
-        
-        if a not in network.keys():
-            network[a]=[b,]
-        #elif b not in network[a]:
-        #    network[a].append(b)
-        else:
-            network[a].append(b)
-            network[a] = list(set(network[a]))
-        if b not in network.keys():
-            network[b]=[a,]
-        #elif a not in network[b]:
-        #    network[b].append(a)
-        else:
-            network[b].append(a)
-            network[b] = list(set(network[b]))
+        network[a-1] = list(set(network[a-1]+[b]))
+        network[b-1] = list(set(network[b-1]+[a]))
 
-    # count how many computers are infected 
-    # recursion function call
-    # 개선2
-    count = recur_worm(network, 1, [1])
-    print(count)
-    #print(time.time()-s)
+    visited = list()
+    q.put(1)
+    cur_node = 1
+    
+    while not q.empty():
+        cur_node = q.get()
+        #print(f"[=>]current node {cur_node}")
+        for i in network[cur_node-1]:
+            #print(f"[+] {i} >> Queue")
+            q.put(i)
+            
+        if cur_node not in visited:
+            visited.append(cur_node)
+            #del network[cur_node-1][:]
+            network[cur_node-1].clear()
 
+    #print(network)
 
+    print(len(visited)-1)
+#    print(time.time()-s)
+    
