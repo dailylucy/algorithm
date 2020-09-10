0ce1fbfc09228ab48bdff8252b3406ff23550123
k1ndS2 <61953345+k1ndS2@users.noreply.github.com>
2020-09-10 19:31:02 +0900

    Create 2644.cpp

---
+++ baekjoon/_dfs/2644.cpp
@@ -0,0 +1,48 @@
+// 촌수 계산
+// https://www.acmicpc.net/problem/2644
+// C++ 14
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M, end1, end2;
+int mincnt = 101;
+
+
+vector<vector<int>> graph(101);
+bool isVisited[101] = { false };
+
+void addEdges() 
+{
+	int tmp1, tmp2;
+	for (auto i = 0; i < M; i++) 
+	{
+		cin >> tmp1 >> tmp2;
+		graph[tmp1].push_back(tmp2);
+		graph[tmp2].push_back(tmp1);
+	}
+}
+
+void dfs(int node, int sum) 
+{
+	if (isVisited[node]) {  return; }
+	sum++;
+	isVisited[node] = true;
+	if (node == end2) { if (mincnt > sum) mincnt = sum; sum--; return; }
+
+	for (auto i = 0; i < graph[node].size(); i++) {
+		dfs(graph[node][i], sum);
+	}
+    sum--;
+}
+
+int main() {
+	cin >> N >> end1 >> end2 >> M;
+	addEdges();
+	dfs(end1, 0);
+	if (mincnt == 101) cout << -1 << endl;
+	else	             cout << mincnt-1 << endl;
+	return 0;
+}
