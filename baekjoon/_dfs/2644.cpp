84116a1f9bb54586e3223d4ac17832ae536b5ccd
k1ndS2 <61953345+k1ndS2@users.noreply.github.com>
2020-09-10 19:49:20 +0900

    Update 2644.cpp

--- baekjoon/_dfs/2644.cpp
+++ baekjoon/_dfs/2644.cpp
@@ -37,8 +37,28 @@ void dfs(int node, int sum)
 	for (auto i = 0; i < graph[node].size(); i++) {
 		dfs(graph[node][i], sum);
 	}
-    sum--;
+	sum--;
 }
+
+/* 위와 조금 다르지만 정답나온 dfs 함수 코드.
+ * 현재 노드가 아닌 자식 노드를 확인한 후 자식노드로 갈 때, sum을 증가시킴.
+ * 이 경우 main에서 mincnt를 그대로 출력한다. 즉, 이 함수를 위의 dfs 대신 사용시
+ * [68 line - main에서 출력부분] else cout << mincnt << endl; 로 변경할 것
+ 
+void dfs(int node, int sum) 
+{
+	if (isVisited[node]) {  return; }
+	
+	isVisited[node] = true;
+	if (node == end2) { if (mincnt > sum) mincnt = sum; sum--; return; }
+
+	for (auto i = 0; i < graph[node].size(); i++) {
+		sum++;
+		dfs(graph[node][i], sum);
+		sum--;
+	}
+}
+*/
 
 int main() {
 	cin >> N >> end1 >> end2 >> M;
