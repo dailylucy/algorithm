7c136ada8a57c3d42549ae736ccee489146df2ff
dailylucy <61953345+dailylucy@users.noreply.github.com>
2020-11-30 23:13:05 +0900

    Update [2019]kakao_winter_internship.cpp

--- programmers/[2019]kakao_winter_internship.cpp
+++ programmers/[2019]kakao_winter_internship.cpp
@@ -11,26 +11,28 @@ int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
     int now = 0;
     vector<int> bucket;
+    int prev = -1;
     for(int i = 0 ; i < moves.size(); i++){
         now = moves[i]-1;
         for(int r = 0 ; r < board.size(); r++){
             if(board[r][now]) {
-                // bucket에 담기
-                bucket.push_back(board[r][now]);
-                // board 변경
-                board[r][now] = 0;
-                
-                // 이전과 같은지
-                int bucketSz = bucket.size();
-                if(bucketSz > 1 && bucket[bucketSz-2] == bucket[bucketSz-1]){                    
-                    bucket.pop_back();
+                // 첫 move?
+                // 이전 노드와 현재 비교
+                if(prev != -1 && prev == board[r][now]) {
+                    // 같으면 제거
                     bucket.pop_back();
                     answer = answer + 2;
                 }
+                else{
+                    // bucket에 담기
+                    bucket.push_back(board[r][now]);            
+                }
+                // board 변경
+                board[r][now] = 0;
+                prev = bucket[bucket.size()-1];        
                 break;
             }
         }
-        
     }
     return answer;
 }
