2fa3ce1fe9cf565acdf6c628a92e2435b13b43c9
dailylucy <61953345+dailylucy@users.noreply.github.com>
2020-12-03 11:31:24 +0900

    Create skill_tree.cpp

---
+++ programmers/skill_tree.cpp
@@ -0,0 +1,32 @@
+
+// 스킬트리
+// https://programmers.co.kr/learn/courses/30/lessons/49993#
+ 
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    
+    for (auto user_skill : skill_trees) {
+        int prevTreeIdx = -2;
+        int nowTreeIdx = -2;
+        for (int sIdx = 0; sIdx < skill.size(); sIdx++) {
+             nowTreeIdx = user_skill.find(skill[sIdx]);   
+             // 이전 스킬이 지금 스킬보다 뒤에 나옴.
+             if ( nowTreeIdx > -1 && prevTreeIdx > nowTreeIdx ) break;
+             // 이전 스킬이 없는데 지금 스킬이 있는 경우
+             if( nowTreeIdx > -1 && prevTreeIdx == -1) break;
+             if( sIdx == skill.size()-1 /*|| prevTreeIdx == user_skill.size()-1*/){
+                 answer++;
+                 break;
+             } 
+             
+             prevTreeIdx = nowTreeIdx;
+        }
+    }
+    return answer;
+}
